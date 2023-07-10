9c0db9e092f9d795b9ee6409f13f32e34f9c142b
Nikolas Korasidis <n.korasidis@silentech.gr>
2023-07-10 14:44:02 +0300

    Register only Preprocess pipeline

--- src/hedno/pipeline_registry.py
+++ src/hedno/pipeline_registry.py
@@ -4,13 +4,18 @@ from __future__ import annotations
 from kedro.framework.project import find_pipelines
 from kedro.pipeline import Pipeline
 
+import hedno.pipelines.preprocess as prp
 
-def register_pipelines() -> dict[str, Pipeline]:
+
+def register_pipelines() -> Dict[str, Pipeline]:
     """Register the project's pipelines.
 
     Returns:
         A mapping from pipeline names to ``Pipeline`` objects.
     """
-    pipelines = find_pipelines()
-    pipelines["__default__"] = sum(pipelines.values())
-    return pipelines
+    preprocess_pipeline = prp.create_pipeline()
+
+    return {
+        "__default__": preprocess_pipeline,
+        "preprocess": preprocess_pipeline,
+    }
